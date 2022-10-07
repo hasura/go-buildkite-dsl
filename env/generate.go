93e05f6a087ccf860744a587cf543365b16c837a
Vishnu Bharathi P <vishnubharathi04@gmail.com>
2022-10-07 06:50:27 +0530

    env: collect all buildkite related envs from docs

--- env/generate.go
+++ env/generate.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/html"
 )
+
+type env struct {
+	name string
+}
 
 func main() {
 	resp, err := http.Get("https://buildkite.com/docs/pipelines/environment-variables")
@@ -12,10 +18,34 @@ func main() {
 		log.Fatalf("error downloading buildkite docs: %+v", err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+
+	doc, err := html.Parse(resp.Body)
 	if err != nil {
-		log.Fatalf("error while reading response: %+v", err)
+		log.Fatalf("error parsing html: %+v", err)
 	}
 
-	log.Printf("body: %s", string(body))
+	var envs []env
+	var traverseHTML func(*html.Node)
+	traverseHTML = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "tr" {
+			var _env *env
+			for _, attr := range n.Attr {
+				if attr.Key == "id" {
+					_env = &env{
+						name: attr.Val,
+					}
+				}
+			}
+
+			if _env != nil {
+				envs = append(envs, *_env)
+			}
+		}
+
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverseHTML(c)
+		}
+	}
+	traverseHTML(doc)
+	fmt.Printf("+%v", envs)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hasura/go-buildkite-dsl
 
 go 1.16
+
+require golang.org/x/net v0.0.0-20221004154528-8021a29435af // indirect
